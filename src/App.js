77a77a9084b19e2a91fd0ec430a77adfb8429848
Jill Kidman <jillkidman@gmail.com>
2022-07-05 13:49:59 -0700

    intital commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { Route, Routes, Link } from 'react-router-dom';
+import BlogsPage from './Pages/Blogs';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+const urlEndpoint = "http://localhost:4000";
+
+
+function App() {
+  const [serverJSON, setServerJSON] = useState({ message: null });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const apiResponse = await fetch(`${urlEndpoint}/blogs/hello-blogs`);
+      const apiJSON = await apiResponse.json();
+      setServerJSON(apiJSON);
+      return;
+    };
+    fetchData();
+  }, []);
+
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/blogs"
+          element={<BlogsPage message={serverJSON.message} />}></Route>
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
